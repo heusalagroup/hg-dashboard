97b0b2608ce9ccb85c98c8709fa7ea55b5c8e8d2
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-10 12:25:15 +0300

    Fixing dashboard login

--- frontend/src/services/AppInitService.ts
+++ frontend/src/services/AppInitService.ts
@@ -2,6 +2,8 @@
 
 import { LogService } from "../fi/hg/core/LogService";
 import { WorkspaceService } from "./WorkspaceService";
+import { EmailAuthHttpService } from "../fi/hg/frontend/services/EmailAuthHttpService";
+import { DASHBOARD_AUTHENTICATE_EMAIL_URL_WITH_LANGUAGE, DASHBOARD_VERIFY_EMAIL_CODE_URL_WITH_LANGUAGE, DASHBOARD_VERIFY_EMAIL_TOKEN_URL_WITH_LANGUAGE } from "../fi/hg/dashboard/constants/dashboard-api";
 
 const LOG = LogService.createLogger('AppInitService');
 
@@ -10,6 +12,11 @@ export class AppInitService {
     public static initialize () {
         try {
             LOG.debug(`Initializing app...`);
+            EmailAuthHttpService.initialize(
+                (lang) => '/api' + DASHBOARD_AUTHENTICATE_EMAIL_URL_WITH_LANGUAGE(lang),
+                (lang) => '/api' + DASHBOARD_VERIFY_EMAIL_CODE_URL_WITH_LANGUAGE(lang),
+                (lang) => '/api' + DASHBOARD_VERIFY_EMAIL_TOKEN_URL_WITH_LANGUAGE(lang)
+            );
             WorkspaceService.initialize();
             LOG.info(`App initialized.`);
         } catch (err) {
